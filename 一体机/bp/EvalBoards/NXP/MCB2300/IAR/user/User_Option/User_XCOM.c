df6a6c3002a2211f116725362245db6760172dc3
wcf873455992 <wcf873455992@sina.com>
2015-12-29 13:35:57 +0800

    3.05 修复投影机串口码发送间隔太短导致关机失败

--- 一体机/bp/EvalBoards/NXP/MCB2300/IAR/user/User_Option/User_XCOM.c
+++ 一体机/bp/EvalBoards/NXP/MCB2300/IAR/user/User_Option/User_XCOM.c
@@ -809,7 +809,8 @@ void TASK_UART1Send(void *pdata)
         {
             pxrev = (struct XCOM_REV_TYPE *)p;
             UART_Send_Data(pxrev->XcomPort, p + sizeof(struct XCOM_REV_TYPE), pxrev->XcomLengh);
-            OSTimeDly(1);
+            //OSTimeDly(1);
+            OSTimeDlyHMSM(0, 0, 1, 0);//add wcf 修复发送间隔太短导致关机失败
             User_mem_free(p);
         }
 
